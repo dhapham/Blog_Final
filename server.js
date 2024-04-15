085921bee32c5810a00471817f46d5c7a57e3889
John Nguyen <46435166+johnng0805@users.noreply.github.com>
2024-04-16 02:56:19 +0700

    dynamic port

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ async function SetupServer() {
     });
 
     // Start the server
-    const PORT = 3000;
+    const PORT = process.env.PORT || 3000;
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
